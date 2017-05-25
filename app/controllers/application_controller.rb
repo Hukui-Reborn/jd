4e7e508b79fe0488db3662908cc498225f071f47
xyy601chenyan <xyy545688506@gmail.com>
2017-05-25 17:23:49 +0800

    user can update account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def admin_required
     if !current_user.admin?
       redirect_to "/", alert: "You are not admin."
@@ -10,7 +12,21 @@ class ApplicationController < ActionController::Base
   def current_cart
     @current_cart ||= find_cart
   end
+
+
+  protected
+  def configure_permitted_parameters
+    added_attrs = [:user_name, :email, :password, :password_confirmation, :remember_me]
+    added_params = [:user_name, :email, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+   devise_parameter_sanitizer.permit :account_update, keys: added_params
+ end
+
+
+
   private
+
+
 
   def find_cart
     cart = Cart.find_by(id: session[:cart_id])
