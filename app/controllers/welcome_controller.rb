af8b0312bed1dae4ae15a010dda042d51656d93d
Hukui <tckuikui1234@gmail.com>
2017-06-05 22:48:01 +0800

    change welcome style

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-     result=Product.where("id<?",Product.last.id-5).order("created_at DESC")
+     result=Product.where("id<8",Product.last.id-5).order("created_at DESC")
      @datas = result.paginate(:page => params[:page], :per_page => 8)
   end
 end
